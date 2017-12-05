300049537128c44bc33d894fbde05ccd806c8d2f
ClaraAmeller <claraameller7@gmail.com>
2017-12-05 16:52:32 +0100

    Create servies/ingredient, pages/home, app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
+import { IngredientService } from './services/ingredient.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  // { path: 'journal/:id', component: JournalDetailComponent },
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +20,13 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    IngredientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
